fc04a25d5bddb185d183ce7275796d25ded2c247
LighteningCode <elvisagbesi@gmail.com>
2022-04-03 14:00:02 +0000

    feat: added data needed,

---
+++ data.js
@@ -0,0 +1,218 @@
+
+exports.articles = [
+    {
+        id: 1,
+        title: "A Lie Gets Halfway Around Social Media While the Truth Is Still Getting Its Boots On",
+        authorId: 5,
+        image: "https://miro.medium.com/fit/c/200/134/1*1jX1O5nNl2Kza7Pf0h0WeA.jpeg",
+        readTime: 8,
+        date: "Feb 19",
+        subTitle: "Why did so many people think a blatantly fake apology from Chris Rock was real?",
+        tags: ["Thought"]
+    },
+    {
+        id: 2,
+        title: "I’m Not Photographing That",
+        authorId: 1,
+        image: "https://miro.medium.com/fit/c/200/134/1*8RIWPIqZLH-E6FYscNTK1g.jpeg",
+        readTime: 3,
+        date: "Mar 20",
+        subTitle: "The Constant Negotiation Between Living and Curating",
+        tags: ["Photography"]
+    },
+    {
+        id: 3,
+        title: "Why It’s Hard to Identify Toxic from Productive Content",
+        authorId: 1,
+        image: "https://miro.medium.com/fit/c/200/134/1*hFZDmCYocMpsBfb85TbPdg.png",
+        readTime: 6,
+        date: "Jan 13",
+        subTitle: "Using mindfulness to be better about the content we pay attention to",
+        tags: ["Social Media"]
+    },
+    {
+        id: 4,
+        title: "Today’s ‘Lazy Generation’ Needed the Great Resignation",
+        authorId: 2,
+        image: "https://miro.medium.com/fit/c/200/134/0*zVg7u1YB49LiVC6-",
+        readTime: 10,
+        date: "Mar 23",
+        subTitle: "Millennials have turned their obstacle into an opportunity",
+        tags: ["Generation"]
+    },
+    {
+        id: 5,
+        title: "The Crypto Bubble Is Starting to Burst",
+        authorId: 2,
+        image: "https://miro.medium.com/fit/c/200/134/1*LRP68L7dvDQxCqmxKgLWMw.jpeg",
+        readTime: 6,
+        date: "Mar 22",
+        subTitle: "It’s time to stop beating around the bush",
+        tags: ["Crypto"]
+    },
+    {
+        id: 6,
+        title: "The Real Reason To ‘Learn To Code’? Automating Your Life",
+        authorId: 2,
+        image: "https://miro.medium.com/fit/c/200/134/1*FktCcTeiX1wikEVWz_FqMg.jpeg",
+        readTime: 3,
+        date: "Apr 15",
+        subTitle: "On the joys of writing scripts that do scut work",
+        tags: ["Programming"]
+    },
+    {
+        id: 7,
+        title: "My Two and a Half Weeks of Life and Death in Mariupol",
+        authorId: 6,
+        image: "https://miro.medium.com/fit/c/200/134/1*lFhsA1jGgE2OpyRPH1aLTw.jpeg",
+        readTime: 6,
+        date: "Feb 16",
+        subTitle: "We did unthinkable things to survive.",
+        tags: ["Survive"]
+    },
+    {
+        id: 8,
+        title: "The Question That Frees Up a Schedule",
+        authorId: 3,
+        image: "https://miro.medium.com/fit/c/200/134/0*jiiXubvil8zQC_sJ",
+        readTime: 5,
+        date: "Feb 19",
+        subTitle: "Raise the chances that when you do whatever you agreed to do, you’ll actually want to do it",
+        tags: ["Time Management"]
+    },
+    {
+        id: 9,
+        title: "Philosophy Needs To Diversify, And We Need To Know How",
+        authorId: 5,
+        image: "https://miro.medium.com/fit/c/200/134/0*rGan1M9rL6f6CGKS",
+        readTime: 7,
+        date: "Jan 12",
+        subTitle: "Why is philosophy so hard to diversify? And what can we do about it?",
+        tags: ["Philosophy"]
+    },
+    {
+        id: 10,
+        title: "It’s Getting a Little Crowded",
+        authorId: 1,
+        image: "https://miro.medium.com/fit/c/200/134/1*HJz1ygEsTYTyfwhPfQXNtg.jpeg",
+        readTime: 10,
+        date: "Jan 22",
+        subTitle: "Academia’s Problem With Centering Themselves in Someone Else’s Lived Experience",
+        tags: ["Education"]
+    },
+    {
+        id: 11,
+        title: "The Truth About Employment Contracts",
+        authorId: 4,
+        image: "https://miro.medium.com/fit/c/200/134/1*N1wrfgbxb5xDr7z3mHzfpg.jpeg",
+        readTime: 6,
+        date: "Apr 6",
+        subTitle: "And how you can escape the trap",
+        tags: ["Work"]
+    },
+    {
+        id: 12,
+        title: "Is Aging a Disease or a Natural Process?",
+        authorId: 9,
+        image: "https://miro.medium.com/fit/c/200/134/1*lPBFhhhREWLX9GpDnPncMg.jpeg",
+        readTime: 4,
+        date: "Aug 12",
+        subTitle: "Mystery, stigma and dreams of immortality fuel a debate that just won’t die",
+        tags: ["Aging"]
+    },
+    {
+        id: 13,
+        title: "A Guide to Running Away From Your Problems",
+        authorId: 6,
+        image: "https://miro.medium.com/freeze/fit/c/200/134/1*sL9jxg1S3HcSz94OKCKdAA.gif",
+        readTime: 3,
+        date: "Jun 22",
+        subTitle: "Avoiding your problems is easy but is it wise?",
+        tags: ["Humor"]
+    },
+    {
+        id: 14,
+        title: "The Highway Projects That Almost Destroyed Copenhagen",
+        authorId: 5,
+        image: "https://miro.medium.com/fit/c/200/134/1*zyfpV7mSxk7FaruaPRJM8g.png",
+        readTime: 9,
+        date: "May 3",
+        subTitle: "Copenhagen planned massive highway projects between the 1950s and 1970s. With new projects planned, we need to",
+        tags: ["Roads"]
+    },
+    {
+        id: 15,
+        title: "Why I Turned Off Comments and DMs",
+        authorId: 3,
+        image: "https://miro.medium.com/fit/c/200/134/0*n1UUiKnY7LoeeHIq",
+        readTime: 8,
+        date: "Jul 5",
+        subTitle: "My social media accounts have been on strict lockdown for the last few months. I’ve never known such peace",
+        tags: ["Social"]
+    },
+];
+
+
+
+exports.authors = [
+    {
+        id: 1,
+        portfolio:"Technologist",
+        name: "Tim Wise",
+        image: "https://cdn-static-1.medium.com/sites/medium.com/about/images/Erica_Dhawan_clip.png"
+    },
+    {
+        id: 2,
+        portfolio:"Artist",
+        name: "Miriam Buss",
+        image: "https://cdn-static-1.medium.com/sites/medium.com/about/images/About_Bina-1.png"
+    },
+    {
+        id: 3,
+        portfolio:"Best Selling Author",
+        name: "Devon Prince",
+        image: "https://cdn-static-1.medium.com/sites/medium.com/about/images/DevonPrice_1DevonPrice.png"
+    },
+    {
+        id: 4,
+        portfolio:"Google Engineer",
+        name: "Scott Galloway",
+        image: "https://cdn-static-1.medium.com/sites/medium.com/about/images/tri-col-item-04_1tri-col-item-04.png"
+    },
+    {
+        id: 5,
+        portfolio:"Creative Writer",
+        name: "Ming Ui",
+        image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 6,
+        portfolio:"Artist",
+        name: "Robert Roy Britt",
+        image: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 7,
+        portfolio:"Clinical Doctor",
+        name: "Clive Thompson",
+        image: "https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+    },
+    {
+        id: 8,
+        portfolio:"Economist",
+        name: "Charlie Cole",
+        image: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 9,
+        portfolio:"Teacher & Education",
+        name: "Tara Haelle",
+        image: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 10,
+        portfolio:"Blogger",
+        name: "James Surowiecki",
+        image: "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+    },
+];
